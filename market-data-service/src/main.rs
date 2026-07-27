77edb6c323ad388ae1b53fc5bb002edd0ede6a51
Alexander Bulatov <aleks40k@yandex.ru>
2026-07-27 18:20:20 +0300

    feat(market-data): prepare some code to fetch market data from api

--- market-data-service/src/main.rs
+++ market-data-service/src/main.rs
@@ -1,5 +1,43 @@
-use shared::messages::grpc::tradeapi::v1::marketdata::BarsRequest;
+use shared::messages::grpc::tradeapi::v1::marketdata::SubscribeBarsRequest;
+use shared::messages::grpc::tradeapi::v1::marketdata::TimeFrame;
+use shared::messages::grpc::tradeapi::v1::marketdata::market_data_service_client;
+use tonic::Request;
+use tonic::metadata::MetadataValue;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client =
+        market_data_service_client::MarketDataServiceClient::connect("http://api.finam.ru:443")
+            .await?;
+
+    let payload = SubscribeBarsRequest {
+        symbol: "IMOEXF".to_string(),
+        timeframe: TimeFrame::M1 as i32,
+    };
+
+    let mut request = Request::new(payload);
+
+    let token: MetadataValue<_> = "token_here".parse().unwrap();
+    request.metadata_mut().insert("secret", token);
+
+    let mut response_stream = client.subscribe_bars(request).await?.into_inner();
+
+    loop {
+        let res = response_stream.message().await;
+
+        match res {
+            Ok(resp) => match resp {
+                Some(body) => {
+                    println!("RECEIVED BARS RESPONSE: {:#?}", body.bars);
+                }
+                None => {}
+            },
+            Err(status) => {
+                eprintln!("gRPC error encountered: {}", status);
+                break;
+            }
+        }
+    }
+
+    return Ok(());
 }
--- shared/build.rs
+++ shared/build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Tell Cargo to re-run this script if any .proto file changes
     tonic_prost_build::configure()
         .include_file("mod.rs")
-        .build_server(true) // generate server traits
+        .build_client(true) // generate server traits
         .compile_protos(
             &["protos/finam/grpc/tradeapi/v1/marketdata/marketdata_service.proto"],
             &["protos/finam"],
