8671532dfe02226453adafc3691a38f46cfaecec
Alexander Bulatov <aleks40k@yandex.ru>
2026-07-28 18:18:15 +0300

    feat(shared): add auth_service proto compilation, remove comments

--- shared/build.rs
+++ shared/build.rs
@@ -1,10 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Tell Cargo to re-run this script if any .proto file changes
     tonic_prost_build::configure()
         .include_file("mod.rs")
-        .build_client(true) // generate server traits
+        .build_client(true)
         .compile_protos(
-            &["protos/finam/grpc/tradeapi/v1/marketdata/marketdata_service.proto"],
+            &[
+                "protos/finam/grpc/tradeapi/v1/marketdata/marketdata_service.proto",
+                "protos/finam/grpc/tradeapi/v1/auth/auth_service.proto"
+            ],
             &["protos/finam"],
         )?;
 
