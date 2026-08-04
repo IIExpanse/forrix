1f92fab97971f388b0f23e2a709fe6b0e22ab6f5
Alexander Bulatov <aleks40k@yandex.ru>
2026-08-04 16:45:33 +0300

    feat(market-data): implement auth manager

--- market-data-service/src/application/security/auth_manager.rs
+++ market-data-service/src/application/security/auth_manager.rs
@@ -1,46 +1,77 @@
 use std::sync::{Arc, RwLock};
+use tokio::task::JoinHandle;
+use tonic::Status;
+use tonic::metadata::MetadataValue;
+use tonic::service::Interceptor;
 
 use tracing::info;
 
+use crate::application::errors::app_errors::AppError;
 use crate::application::ports::auth_client;
 
 pub struct AuthManager {
+    token_holder: Arc<RwLock<String>>,
+    handle: JoinHandle<Result<(), AppError>>,
+}
+
+pub struct AuthInterceptor {
     token_holder: Arc<RwLock<String>>,
 }
 
 impl AuthManager {
     pub async fn new(api_url: &str, api_token: &str) -> Self {
-        let instance = AuthManager {
-            token_holder: Arc::new(RwLock::new("".to_owned())),
-        };
+        let token_holder = Arc::new(RwLock::new("".to_owned()));
 
-        let client = match auth_client::FinamAuthGrpcClient::create_grpc_client(api_url).await {
+        let client = match auth_client::FinamAuthGrpcClient::create_client(api_url).await {
             Ok(client) => client,
             Err(e) => panic!("Failed to create auth client: {}", e),
         };
 
-        match auth_client::FinamAuthGrpcClient::start_auth_flow(
+        let handle = match auth_client::FinamAuthGrpcClient::start_auth_flow(
             client,
             api_token,
-            instance.token_holder.clone(),
+            token_holder.clone(),
         )
         .await
         {
-            Ok(_) => info!("Successfully started authentication process."),
+            Ok(res) => {
+                info!("Successfully started authentication process.");
+                res
+            }
             Err(e) => panic!("Failed to initialize authentication flow: {}", e),
-        }
+        };
 
-        instance
+        AuthManager {
+            token_holder,
+            handle,
+        }
     }
 
-    pub fn get_token(&self) -> String {
-        match self.token_holder.read() {
+    pub fn get_auth_interceptor(&self) -> AuthInterceptor {
+        AuthInterceptor {
+            token_holder: self.token_holder.clone(),
+        }
+    }
+}
+
+impl Interceptor for AuthInterceptor {
+    fn call(&mut self, mut request: tonic::Request<()>) -> Result<tonic::Request<()>, Status> {
+        let token = match self.token_holder.read() {
             Ok(res) => res.to_owned(),
             Err(e) => panic!("There is a PoisonError in token lock: {}", e),
-        }
-    }
+        };
 
-    pub fn get_token_holder(&self) -> Arc<RwLock<String>> {
-        self.token_holder.clone()
+        let bearer_token: MetadataValue<_> = MetadataValue::try_from("Bearer ".to_owned() + &token)
+            .expect("Only visible ASCII characters (32-127) are permitted.");
+
+        request.metadata_mut().insert("Authorization", bearer_token);
+
+        Ok(request)
+    }
+}
+
+impl Drop for AuthManager {
+    fn drop(&mut self) {
+        self.handle.abort()
     }
 }
