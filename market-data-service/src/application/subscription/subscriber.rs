4baa929a49188c3acba705ca0c77fcc0c54e2624
Alexander Bulatov <aleks40k@yandex.ru>
2026-08-04 16:50:41 +0300

    feat(market-data): implement bars subscriber

---
+++ market-data-service/src/application/subscription/subscriber.rs
@@ -0,0 +1,119 @@
+use hyper::Uri;
+use shared::messages::grpc::tradeapi::v1::marketdata::{
+    Bar, SubscribeBarsRequest, TimeFrame,
+    market_data_service_client::{self, MarketDataServiceClient},
+};
+use std::str::FromStr;
+use tokio::task::JoinHandle;
+use tonic::{Request, service::interceptor::InterceptedService, transport::Channel};
+use tracing::{error, warn};
+
+use crate::application::{
+    errors::app_errors::AppError,
+    security::auth_manager::{AuthInterceptor, AuthManager},
+};
+
+pub trait Subscriber<T> {
+    async fn subscribe(&mut self) -> Result<(), AppError>;
+
+    fn on_message(&self, message: T);
+}
+
+pub struct BarsSubscriber {
+    market_data_client: MarketDataServiceClient<InterceptedService<Channel, AuthInterceptor>>,
+    subscription_handle: Option<JoinHandle<Result<(), AppError>>>,
+}
+
+impl BarsSubscriber {
+    pub async fn new(
+        api_url: &str,
+        auth_manager: &AuthManager,
+    ) -> Result<BarsSubscriber, AppError> {
+        let channel = Channel::builder(Uri::from_str(api_url).expect("API_URL must be valid."))
+            .connect()
+            .await
+            .map_err(|e| AppError {
+                message: "Failed to open gRPC channel for MarketDataSubscriber".to_owned(),
+                cause: Some(Box::new(e)),
+            })?;
+
+        let market_data_client =
+            market_data_service_client::MarketDataServiceClient::with_interceptor(
+                channel,
+                auth_manager.get_auth_interceptor(),
+            );
+        Ok(BarsSubscriber {
+            market_data_client,
+            subscription_handle: None,
+        })
+    }
+}
+
+impl Subscriber<&Bar> for BarsSubscriber {
+    async fn subscribe(&mut self) -> Result<(), AppError> {
+        let sub_request = SubscribeBarsRequest {
+            symbol: "IMOEXF".to_string(),
+            timeframe: TimeFrame::M1 as i32,
+        };
+
+        let mut attempts = 0;
+
+        loop {
+            let request = Request::new(sub_request.clone());
+
+            let resp = self
+                .market_data_client
+                .subscribe_bars(request)
+                .await
+                .map_err(|e| AppError {
+                    message: "Failed to subscribe to bars".to_owned(),
+                    cause: Some(Box::new(e)),
+                });
+
+            if let Err(e) = resp {
+                error!("Error while subscribing to bars: {}", e);
+                attempts += 1;
+
+                if attempts >= 5 {
+                    error!("Connection attempts exceeded for bars stream, aborting");
+                    return Ok(());
+                }
+                continue;
+            }
+            attempts = 0;
+
+            let mut bars_stream = resp.unwrap().into_inner();
+
+            loop {
+                let res = bars_stream.message().await;
+
+                match res {
+                    Ok(resp) => match resp {
+                        Some(body) => {
+                            body.bars.iter().for_each(|bar| self.on_message(bar));
+                        }
+                        None => {
+                            warn!("Received empty bars response.")
+                        }
+                    },
+                    Err(status) => {
+                        error!("Error while waiting for bars message: {}", status);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    fn on_message(&self, message: &Bar) {
+        println!("Received bar: {:#?}", message);
+    }
+}
+
+impl Drop for BarsSubscriber {
+    fn drop(&mut self) {
+        if let Some(handle) = &self.subscription_handle {
+            handle.abort()
+        }
+    }
+}
