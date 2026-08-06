c1c6f622b405b2816ef281f6cf8e8b9e6a286802
Alexander Bulatov <aleks40k@yandex.ru>
2026-08-06 11:35:12 +0300

    refactor(shared): remove template code

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,13 +1,3 @@
 pub mod messages {
     include!(concat!(env!("OUT_DIR"), "/mod.rs"));
 }
-
-// #[cfg(test)]
-// mod tests {
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
