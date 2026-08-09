ae2af5d03f9da3eaea3f867c388e5e38b0b03bca
Alexander Bulatov <aleks40k@yandex.ru>
2026-08-09 19:42:39 +0300

    feat(market-data): add scripts for local deploy

---
+++ scripts/market-data-service/build.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+service_name='market-data-service'
+
+docker stop "$service_name" || true
+docker rm "$service_name" || true
+docker rmi "$service_name" || true
+docker build --progress=plain -t "$service_name" -f ./docker/"$service_name"/Dockerfile .
