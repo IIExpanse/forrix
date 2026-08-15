be6890a3ad605249bc0e5c635d5d4d5a0b646081
Alexander Bulatov <aleks40k@yandex.ru>
2026-08-15 17:41:15 +0300

    feat(control): prepare bot commands enum

--- control-service/src/domain/telegram/tg_bot.rs
+++ control-service/src/domain/telegram/tg_bot.rs
@@ -1,14 +1,33 @@
-pub enum InfoState {
-    Start,
-    GetSystemState,
-    GetCurrentConfig,
-    GetChosenInstrumentInfo { ticker: str },
-}
+use teloxide::utils::command::BotCommands;
 
-pub enum CommandState {
-    SetInstrument { ticker: str, mic: str },
+#[derive(BotCommands, Clone, Default)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+pub enum Command {
+    // informational
+    #[default]
+    #[command(description = "Display this text.")]
+    Start,
+    #[command(description = "Show system health status.")]
+    GetSystemHealth,
+    #[command(description = "Show all properties and their values.")]
+    GetCurrentConfig,
+    #[command(description = "Show detailed information about chosen instrument.")]
+    GetChosenInstrumentInfo { ticker: String },
+    // configurational
+    #[command(description = "Set instrument to be traded.")]
+    SetInstrument { ticker: String },
+    #[command(description = "Set market identifier code for chosen stock exchange.")]
+    SetMic { mic: String },
+    #[command(description = "Set amount to be traded in 1 operation.")]
     SetAmount { amount: u32 },
+    // trade operations
+    #[command(description = "Execute buy operation.")]
     Buy,
+    #[command(description = "Execute sell operation.")]
     Sell,
+    #[command(description = "Close current position.")]
     ClosePosition,
 }
