5c23e7f038b8604275e9b4f03718d7acb7005a56
Alexander Bulatov <aleks40k@yandex.ru>
2026-08-16 15:39:41 +0300

    feat(control): add command logic code for later implementation

--- control-service/src/domain/telegram/tg_bot.rs
+++ control-service/src/domain/telegram/tg_bot.rs
@@ -1,4 +1,17 @@
-use teloxide::utils::command::BotCommands;
+use teloxide::{
+    Bot,
+    dispatching::{
+        Dispatcher, HandlerExt, UpdateFilterExt, UpdateHandler,
+        dialogue::{Dialogue, InMemStorage},
+    },
+    dptree,
+    repls::CommandReplExt,
+    requests::{Requester, ResponseResult},
+    types::{Message, Update},
+    utils::command::BotCommands,
+};
+
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 
 #[derive(BotCommands, Clone, Default)]
 #[command(
@@ -30,4 +43,72 @@ pub enum Command {
     Sell,
     #[command(description = "Close current position.")]
     ClosePosition,
+}
+
+struct TgChat {
+    ticker: Option<String>,
+    mic: Option<String>,
+    amount: Option<u32>,
+}
+
+// todo refactor one-arg setter functions into generic
+impl TgChat {
+    async fn start(bot: Bot, msg: Message) -> HandlerResult {
+        bot.send_message(msg.chat.id, Command::descriptions().to_string())
+            .await?;
+        Ok(())
+    }
+
+    async fn set_instrument(&mut self, bot: Bot, msg: Message, ticker: String) -> HandlerResult {
+        self.ticker = Some(ticker.clone());
+
+        bot.send_message(msg.chat.id, format!("Saved: ticker = {}", ticker))
+            .await?;
+
+        Ok(())
+    }
+
+    async fn set_mic(&mut self, bot: Bot, msg: Message, mic: String) -> HandlerResult {
+        self.mic = Some(mic.clone());
+
+        bot.send_message(msg.chat.id, format!("Saved: mic = {}", mic))
+            .await?;
+
+        Ok(())
+    }
+
+    async fn set_amount(&mut self, bot: Bot, msg: Message, amount: u32) -> HandlerResult {
+        self.amount = Some(amount);
+
+        bot.send_message(msg.chat.id, format!("Saved: amount = {}", amount))
+            .await?;
+
+        Ok(())
+    }
+
+    // async fn commands_handler(bot: Bot, msg: Message, cmd: Command) {
+    //     let handler = Update::filter_message().endpoint(|bot: Bot, msg: Message| async move {
+    //         let cmd: Command = Command::parse(msg.text().unwrap(), "bot_username").unwrap();
+    //         match cmd {
+    //             Command::Start => todo!(),
+    //             Command::GetSystemHealth => todo!(),
+    //             Command::GetCurrentConfig => todo!(),
+    //             Command::GetChosenInstrumentInfo { ticker } => todo!(),
+    //             Command::SetInstrument { ticker } => todo!(),
+    //             Command::SetMic { mic } => todo!(),
+    //             Command::SetAmount { amount } => todo!(),
+    //             Command::Buy => todo!(),
+    //             Command::Sell => todo!(),
+    //             Command::ClosePosition => todo!(),
+    //         }
+    //     });
+
+    //     Dispatcher::builder(bot, handler)
+    //         // Pass the shared state to the handler as a dependency.
+    //         .dependencies(dptree::deps![messages_total])
+    //         .enable_ctrlc_handler()
+    //         .build()
+    //         .dispatch()
+    //         .await;
+    // }
 }
